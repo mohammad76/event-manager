1dda1a2c48c50d38faa5e09046176dc42f47d926
Mohammad <mohammad_abdi76@yahoo.com>
2020-03-01 22:23:34 +0330

    update routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,7 +42,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiV1Routes();
+        $this->mapApiRoutes();
 
         $this->mapWebRoutes();
 
@@ -70,11 +70,11 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function mapApiV1Routes()
+    protected function mapApiRoutes()
     {
-        Route::prefix('api/v1')
-            ->middleware('api')
-            ->namespace($this->namespace .'\Api\v1')
-            ->group(base_path('routes/api/v1.php'));
+        Route::prefix('api')
+             ->middleware('api')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/api.php'));
     }
 }
